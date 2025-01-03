536aafb5b31274dfba4a4b640e11e1beee4a8344
Turtle <rainy.turtle@icloud.com>
2025-01-03 08:31:30 -0500

    Added the bookmarklet source code
    Added the bookmarklet demo source code, to be referenced by the actual code the user will use in their url.

---
+++ tutorialExampleUse.js
@@ -0,0 +1,6 @@
+javascript:(function(){
+    const script=document.createElement('script');
+    script.src='https://raw.githubusercontent.com/glowingTurtle/Gimkit-Documentation/main/tutorialExample.js';
+    script.crossOrigin='anonymous';
+    document.body.appendChild(script);
+})();
